d6607450173cc2adee6de09fe56fe186bb3112fd
Wolfmarsh <john.marsh@gmail.com>
2017-08-06 22:22:48 -0400

    changed the modes around to be managed by a larger modemanager mode and also moved sound and graphics to an assetmanager mode for the main groups

--- modes/sbone_servoactivation/code/sbone_servoactivation.py
+++ modes/sbone_servoactivation/code/sbone_servoactivation.py
@@ -18,16 +18,16 @@ class sbone_servoactivation(Mode):
         self.machine.events.post('sbone_lock_servo_open')
         
         if self.machine.ball_devices.bd_sbone.available_balls == 3:
-            delay = 150
+            delay = 140
         elif self.machine.ball_devices.bd_sbone.available_balls == 2:
-            delay = 180
+            delay = 160
         elif self.machine.ball_devices.bd_sbone.available_balls == 1:
             delay = 210
         else:
             delay = 250
 
         self.delay.add(callback=self._post_event, ms=delay, event='sbone_lock_servo_closed')
-        self.delay.add(callback=self._post_event, ms=1000, event='sbone_ball_has_probably_been_released')
+        self.delay.add(callback=self._post_event, ms=1200, event='sbone_ball_has_probably_been_released')
 
     def _post_event(self, event):
         self.machine.events.post(event)
